4dbfeb85a3f20d8c7f47ac94410dd08c6b5eecb7
Miko <mikosong2013@gmail.com>
2018-06-12 01:04:28 +0800

    ADD: cpp project: thread pool2

--- cpp_project/c11-project/ThreadPool2cpp.cpp
+++ cpp_project/c11-project/ThreadPool2cpp.cpp
@@ -45,6 +45,7 @@ public:
     function_wrapper& operator=(const function_wrapper&) = delete;
 };
 
+//清单9.2 可等待任务的线程池
 class thread_pool
 {
     std::atomic_bool done;
@@ -120,6 +121,7 @@ public:
         }
     }
 };
+//清单9.7 基于锁的任务窃取队列
 class thread_pool2
 {
     ThreadSafeQueue<function_wrapper> pool_work_queue;
@@ -200,6 +202,158 @@ public:
         }
     }
     // rest as before
+};
+
+//清单9.7 基于锁的任务窃取队列
+class work_stealing_queue
+{
+private:
+    typedef function_wrapper data_type;
+    std::deque<data_type> the_queue; // 1
+    mutable std::mutex the_mutex;
+public:
+    work_stealing_queue()
+    {}
+    work_stealing_queue(const work_stealing_queue& other) = delete;
+    work_stealing_queue& operator=(
+        const work_stealing_queue& other) = delete;
+    void push(data_type data) // 2
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        the_queue.push_front(std::move(data));
+    }
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        return the_queue.empty();
+    }
+    bool try_pop(data_type& res) // 3
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        if (the_queue.empty())
+        {
+            return false;
+        }
+        res = std::move(the_queue.front());
+        the_queue.pop_front();
+        return true;
+    }
+    bool try_steal(data_type& res) // 4
+    {
+        std::lock_guard<std::mutex> lock(the_mutex);
+        if (the_queue.empty())
+        {
+            return false;
+        }
+        res = std::move(the_queue.back());
+        the_queue.pop_back();
+        return true;
+    }
+};
+
+//清单9.8 使用任务窃取的线程池
+class thread_pool3
+{
+    typedef function_wrapper task_type;
+    std::atomic_bool done;
+    ThreadSafeQueue<task_type> pool_work_queue;
+    std::vector<std::unique_ptr<work_stealing_queue> > queues; //1
+        std::vector<std::thread> threads;
+    JoinThreads joiner;
+    static thread_local work_stealing_queue* local_work_queue; //2
+
+    static thread_local unsigned my_index;
+    void worker_thread(unsigned my_index_)
+    {
+        my_index = my_index_;
+        local_work_queue = queues[my_index].get(); // 3
+        while (!done)
+        {
+            run_pending_task();
+        }
+    }
+
+    bool pop_task_from_local_queue(task_type& task)
+    {
+        return local_work_queue && local_work_queue->try_pop(task);
+    }
+    bool pop_task_from_pool_queue(task_type& task)
+    {
+        auto ret = pool_work_queue.TryPop(task);
+        return ret.get();
+    }
+    bool pop_task_from_other_thread_queue(task_type& task) // 4
+    {
+        for (unsigned i = 0; i < queues.size(); ++i)
+        {
+            unsigned const index = (my_index + i + 1) % queues.size(); // 5
+            if (queues[index]->try_steal(task))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+public:
+    thread_pool3() :
+        done(false), joiner(threads)
+    {
+        unsigned const
+            thread_count = std::thread::hardware_concurrency();
+        try
+        {
+            for (unsigned i = 0; i < thread_count; ++i)
+            {
+                queues.push_back(std::unique_ptr<work_stealing_queue>(
+                    // 6
+                    new work_stealing_queue));
+                threads.push_back(
+                    std::thread(&thread_pool3::worker_thread, this, i));
+            }
+        }
+        catch (...)
+        {
+            done = true;
+            throw;
+        }
+    }
+    ~thread_pool3()
+    {
+        done = true;
+    }
+    template<typename FunctionType>
+    std::future<typename std::result_of<FunctionType()>::type>
+        submit(
+            FunctionType f)
+    {
+        typedef typename std::result_of<FunctionType()>::type
+            result_type;
+        std::packaged_task<result_type()> task(f);
+        std::future<result_type> res(task.get_future());
+        if (local_work_queue)
+        {
+            local_work_queue->push(std::move(task));
+        }
+        else
+        {
+            pool_work_queue.push(std::move(task));
+        }
+        return res;
+    }
+    void run_pending_task()
+    {
+        task_type task;
+        if (pop_task_from_local_queue(task) || // 7
+            pop_task_from_pool_queue(task) || // 8
+            pop_task_from_other_thread_queue(task)) // 9
+        {
+            task();
+        }
+        else
+        {
+            std::this_thread::yield();
+        }
+    }
 };
 
 void add()
